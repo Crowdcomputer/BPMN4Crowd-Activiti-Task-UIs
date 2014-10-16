741f8129db891ab29b8f146d829dfe153adc923d
Stefano <stefano.tranquillini@gmail.com>
2014-10-16 10:47:40 +0200

    Add UI for tactic process

---
+++ src/main/java/org/crowdcomputer/ui/tactic/CreateInstance.java
@@ -0,0 +1,52 @@
+package org.crowdcomputer.ui.tactic;
+
+import org.activiti.designer.integration.servicetask.AbstractCustomServiceTask;
+import org.activiti.designer.integration.servicetask.PropertyType;
+import org.activiti.designer.integration.servicetask.annotation.Help;
+import org.activiti.designer.integration.servicetask.annotation.Property;
+import org.activiti.designer.integration.servicetask.annotation.PropertyItems;
+import org.activiti.designer.integration.servicetask.annotation.Runtime;
+
+@Runtime(javaDelegateClass = "org.crowdcomputer.impl.tactic.CreateInstance")
+@Help(displayHelpShort = "Creates a an Instance", displayHelpLong = "Creates instance of the task")
+public class CreateInstance extends AbstractCustomServiceTask {
+
+	// Long process: taken from variable
+	// String title: taken from task def
+
+
+    @Property(type = PropertyType.TEXT, displayName = "Input data variable name", required =  true, defaultValue="data")
+    @Help(displayHelpShort = "Input data variable name", displayHelpLong = "Input data variable name")
+    private String input;
+
+    @Property(type = PropertyType.TEXT, displayName = "Input data variable name", required =  true, defaultValue="data")
+    @Help(displayHelpShort = "Output data variable name", displayHelpLong = "Output data variable name")
+    private String output;
+
+    @Property(type = PropertyType.TEXT, displayName = "Input data execution variable name", required =  true, defaultValue="execution")
+    @Help(displayHelpShort = "Input data execution variable name", displayHelpLong = "Input data variable name")
+    private String input_execution;
+
+    @Property(type = PropertyType.TEXT, displayName = "Ouput data execution variable name", required =  true, defaultValue="execution")
+    @Help(displayHelpShort = "Output data execution variable name", displayHelpLong = "Output data execution variable name")
+    private String output_execution;
+
+	@Override
+	public String getName() {
+		return "Create Instance";
+	}
+	
+	@Override
+	public String getSmallIconPath() {
+		// This is the icon of the component
+		// remember that it is mandatory to provide one.
+		return "icons/cc.png";
+	}
+
+	@Override
+	  public String contributeToPaletteDrawer() {
+	    return "BPMN4Crowd Tactic Def Tasks";
+	  }
+
+
+}
---
+++ src/main/java/org/crowdcomputer/ui/tactic/CreateTask.java
@@ -0,0 +1,73 @@
+package org.crowdcomputer.ui.tactic;
+
+import org.activiti.designer.integration.servicetask.AbstractCustomServiceTask;
+import org.activiti.designer.integration.servicetask.PropertyType;
+import org.activiti.designer.integration.servicetask.annotation.Help;
+import org.activiti.designer.integration.servicetask.annotation.Property;
+import org.activiti.designer.integration.servicetask.annotation.PropertyItems;
+import org.activiti.designer.integration.servicetask.annotation.Runtime;
+
+@Runtime(javaDelegateClass = "org.crowdcomputer.impl.tactic.CreateTask")
+@Help(displayHelpShort = "Creates a Task", displayHelpLong = "Creates a new Task")
+public class CreateTask extends AbstractCustomServiceTask {
+
+	// Long process: taken from variable
+	// String title: taken from task def
+	
+	@Property(type = PropertyType.MULTILINE_TEXT, displayName = "Description of the Task", required = true)
+	@Help(displayHelpShort = "Description", displayHelpLong = "Describe what peopel should do")
+	private String description;	
+	
+	// Date deadline,
+	@Property(type = PropertyType.PERIOD, displayName = "Task duration", required = true)
+	@Help(displayHelpShort = "Select the duration of the task", displayHelpLong = "Duration of the task, if 0 is set to 7 days")
+	private String deadline;
+
+	
+	// String page_url,
+	@Property(type = PropertyType.TEXT, displayName = "Page URL", required = true)
+	@Help(displayHelpShort = "Page URL", displayHelpLong = "Page URL")
+	private String page_url;
+
+
+    @Property(type = PropertyType.TEXT, displayName = "Tactic Process")
+    @Help(displayHelpShort = "Tactic Process", displayHelpLong = "Filenanme")
+    private String tactic_process;
+
+    @Property(type = PropertyType.TEXT, displayName = "Reward", required = true)
+    @Help(displayHelpShort = "Reward", displayHelpLong = "Reward")
+    private String reward;
+    // String reward_platform
+
+    @Property(type = PropertyType.RADIO_CHOICE, displayName = "Reward Platform", required = true)
+    @Help(displayHelpShort = "The maximum daily withdrawl amount ", displayHelpLong = "Choose the maximum daily amount that can be withdrawn from the account.")
+    @PropertyItems({ "CROWDCOMPUTER", "CCM", "DOLLARS", "USD", "EURO", "EUR","COFFEES","COF" })
+    private String platform;
+
+    @Property(type = PropertyType.TEXT, displayName = "Input data name", required =  true, defaultValue="data")
+    @Help(displayHelpShort = "Input data name", displayHelpLong = "Input data name")
+    private String input;
+
+    @Property(type = PropertyType.TEXT, displayName = "Output data name", required =  true, defaultValue="data")
+    @Help(displayHelpShort = "Output data name", displayHelpLong = "Output data name")
+    private String output;
+
+	@Override
+	public String getName() {
+		return "Create Task";
+	}
+	
+	@Override
+	public String getSmallIconPath() {
+		// This is the icon of the component
+		// remember that it is mandatory to provide one.
+		return "icons/cc.png";
+	}
+
+	@Override
+	  public String contributeToPaletteDrawer() {
+	    return "BPMN4Crowd Tactic Def Tasks";
+	  }
+
+
+}
